5d8b5df9e5404027c39e69cfffcd2fb502ce0c05
rizvanovic <teo.rizvanovic@gmail.com>
2022-06-21 17:20:14 +0200

    Added battery print for connection verification (I'm a cave man).

--- testbench.py
+++ testbench.py
@@ -6,12 +6,27 @@ import time, sys, argparse, math
 # Connect to the Vehicle
 print("Connecting")
 connection_string = '/dev/ttyS0'
+wait_ready = input("Wait ready, True or False? \n")
 vehicle = connect(connection_string, wait_ready=False,baud=57600)
 
+print(" Autopilot Firmware version: %s" % vehicle.version)
 
 # Display basic vehicle state
-print(" Type: %s" % vehicle._vehicle_type)
-print(" Armed: %s" % vehicle.armed)
+#print(" Type: %s" % vehicle._vehicle_type)
+#print(" Armed: %s" % vehicle.armed)
 print(" System status: %s" % vehicle.system_status.state)
-print(" GPS: %s" % vehicle.gps_0)
-print(" Alt: %s" % vehicle.location.global_relative_frame.alt)
+#print(" GPS: %s" % vehicle.gps_0)
+#print(" Alt: %s" % vehicle.location.global_relative_frame.alt)
+print("Battery: %s" % vehicle.battery)
+
+
+
+
+
+
+
+
+
+
+
+vehicle.close()
