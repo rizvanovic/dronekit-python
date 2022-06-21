4926f7f09d2a43e8764298c355abb054bb790a43
rizvanovic <teo.rizvanovic@gmail.com>
2022-06-21 17:57:28 +0200

    Channel prints before overrides added.

--- testbench.py
+++ testbench.py
@@ -20,6 +20,16 @@ print(" System status: %s" % vehicle.system_status.state)
 print(" Battery: %s" % vehicle.battery)
 
 input("Press enter to arm. ")
+print("Read channels individually:")
+print(" Ch1: %s" % vehicle.channels['1'])
+print(" Ch2: %s" % vehicle.channels['2'])
+print(" Ch3: %s" % vehicle.channels['3'])
+print(" Ch4: %s" % vehicle.channels['4'])
+print(" Ch5: %s" % vehicle.channels['5'])
+print(" Ch6: %s" % vehicle.channels['6'])
+print(" Ch7: %s" % vehicle.channels['7'])
+print(" Ch8: %s" % vehicle.channels['8'])
+print("Number of channels: %s" % len(vehicle.channels))
 
 vehicle.armed = True
 #cmds = vehicle.commands
