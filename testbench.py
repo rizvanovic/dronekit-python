8b0c605b2e1fadd1c7fa7af35ec074e8ea85866d
rizvanovic <teo.rizvanovic@gmail.com>
2022-06-23 14:43:17 +0200

    Fast shutdown now implemented

--- testbench.py
+++ testbench.py
@@ -61,7 +61,8 @@ while running == True:
         # time.sleep(tt)
     except KeyboardInterrupt:
         print("\n\nKeyboard interrupt: STOPPING NOW.\n\n")
-        change_throttle(0,iNst,motcnt)
+        for iNst in range(6):
+                change_throttle(0, iNst + 1, motcnt)
         time.sleep(1)
         runchoice = input("Would you like to run again? [y/n] ")
         if runchoice == 'n':
