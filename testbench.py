9a803001b920afb2727a9943d610ad709f178f20
rizvanovic <teo.rizvanovic@gmail.com>
2022-06-23 13:44:26 +0200

    Added more functionality for testing.

--- testbench.py
+++ testbench.py
@@ -10,7 +10,7 @@ print("Connecting")
 connection_string = '/dev/ttyS0'
 #wait_ready = input("Wait ready, True or False? \n")
 vehicle = connect(connection_string, wait_ready=False,baud=57600)
-time.sleep(5)
+time.sleep(2.5)
 #vehicle = mavutil.mavlink_connection(connection_string, baud=57600, dialect = "ardupilotmega")
 #print(" Autopilot Firmware version: %s" % vehicle.version)
 # Display basic vehicle state
@@ -22,16 +22,16 @@ time.sleep(5)
 #print(" Battery: %s" % vehicle.battery)
 
 print(f"Aerit test bench, connected to vehicle operating PX4 firmware {vehicle.version}. \n Battery: {vehicle.battery}. ")
-def change_throttle(throttle):
+def change_throttle(throttle, instance, motor_count):
     msg = vehicle.message_factory.command_long_encode(
         0, 0,  # target_system, target_component
         mavutil.mavlink.MAV_CMD_DO_MOTOR_TEST, # command
         0, # confirmation
-        1, # instance
+        instance, # instance
         0, # throttle type
         throttle, # throttle value
         0, # timeout
-        0, # motor count 
+        motor_count, # motor count 
         0, # test order
         0  # empty
         )
@@ -41,20 +41,29 @@ def change_throttle(throttle):
 
 
 running = True
+sttngchce = 'n'
 #PX4setMode(MAV_MODE_OFFBOARD)
 time.sleep(1)
 input("Press enter to start. ")
 while running == True:
+    if sttngchce == 'n':
+        iNst = int(input("Instance: "))
+        motcnt = int(input("Motor count: "))
     try:
         tpc = int(input("Choose throttle % : "))
-       # tt = int(input("For how long?: "))
-        change_throttle(tpc)
+        change_throttle(tpc, iNst, motcnt)
        # print(f"Running for {tt} seconds.")
         #time.sleep(tt)
     except KeyboardInterrupt:
         print("\n\nKeyboard interrupt: STOPPING NOW.\n\n")
         change_throttle(0)
-        running = False
+        time.sleep(1)
+        runchoice = input("Would you like to run again? [y/n] ")
+        if runchoice == 'n':
+            running = False
+        elif runchoice == 'y':
+            sttngchce = input("Same instance and motor count? [y/n]")
+
 
 
 vehicle.close()
